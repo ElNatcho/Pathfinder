3541259475afee75524d6161410b01da95b22b3d
sven <gebhardsven@gmail.com>
2018-02-23 19:26:07 +0100

    Graph Verzeichnisstruktur aus dem Pathfinder-Projekt kopiert.

---
+++ MapCreator/src/Graph/FileMgr/cFileMgr.hpp
@@ -0,0 +1,36 @@
+#ifndef CFILEMGR_HPP
+#define CFILEMGR_HPP
+
+// Includes
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include"../../common.hpp"
+
+// cFileMgr
+class cFileMgr {
+public:
+
+    // -- Kon/Destruktor --
+    cFileMgr();
+    ~cFileMgr();
+
+    // -- Methoden --
+    bool openFile (std::string path); // Versucht eine Datei zu öffnen
+    bool closeFile(); // Versucht die aktuell geöffnete Datei zu schliessen
+
+    bool writeToFile(std::string str); // Versucht einen String in die aktuell geöffnete Datei zu schreiben
+    std::vector<std::string> readFileContent(); // Versucht den Inhalt der aktuell geöffneten Datei auszulesen
+
+private:
+
+    // -- Member Vars --
+    std::fstream *_fstream; // Fstream um mit Dateien zu interagieren
+
+    std::vector<std::string> *_fileContent; // Vector der den Inhalt einer Datei speichert
+    std::string *_tmpString; // Speichert temporär eine aus der aktuell geöffneten Datei ausgelesene Zeile
+
+};
+
+#endif
